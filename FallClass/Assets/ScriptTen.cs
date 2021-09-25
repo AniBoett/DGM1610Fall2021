bc43116c2a1ec2e80e3de6cc424bbb216df2f47a
Anika Boettcher <10945351@my.uvu.edu>
2021-09-24 18:06:46 -0600

    Added ScriptTen1
    ActiveObjects

---
+++ FallClass/Assets/ScriptTen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTen : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
