e845c9b07c1adbd41840c72f5a4f779d78d568df
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:36:57 -0600

    Added Other Variables

---
+++ FallClass/Assets/OtherOperators.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherOperators : MonoBehaviour
+{
+ private int x = 5;
+ x += 3
+  x -= 3
+      x *= 3
+          x /= 3
+              x %= 3
+                  x &= 3
+                      x |= 3
+                          x ^= 3
+                              x >>= 3
+                                  x <<= 3
+}
