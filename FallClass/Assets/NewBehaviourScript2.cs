e516200d732ac0c4c464531854f62721da88230a
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:03:45 -0600

    Added Practice Variables to previous script
    Added variables in comma separated list C#

--- FallClass/Assets/NewBehaviourScript2.cs
+++ FallClass/Assets/NewBehaviourScript2.cs
@@ -6,4 +6,11 @@ public class NewBehaviourScript2 : MonoBehaviour
 {
    private int x = 5, y = 6, z = 50;
    Console.WriteLine(x + y + z);
+   {
+      int myNum = 9; //for single integers
+      double myDoubleNum = 8.99; //for decimals
+      char myLetter = 'A'; //for single characters
+      bool myBoolean = false; //for true false statements
+      string myText = "Hello World"; //for multiple word statements
+   }
 }
