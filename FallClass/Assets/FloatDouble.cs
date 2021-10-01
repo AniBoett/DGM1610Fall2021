7cb565d774206b820f62117caef0b0d5e06a1cbf
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 23:08:53 -0600

    Example FloatDouble

---
+++ FallClass/Assets/FloatDouble.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatDouble : MonoBehaviour
+{
+  private float f1 = 35e3F;
+  private double d1 = 12E4D;
+  Console.Writeline(f1);
+  Console.Writeline(d1);
+}
