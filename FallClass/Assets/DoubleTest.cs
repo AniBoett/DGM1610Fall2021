dfbd572e56d8107d5527f5c2c7e7b4785f0a659f
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 23:06:54 -0600

    Tested Double

---
+++ FallClass/Assets/DoubleTest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleTest : MonoBehaviour
+{
+    private double myNum = 19.99D;
+    Console.WriteLine(myNum);
+}
