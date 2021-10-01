a65729900a4795706f604a0083427e7291aeb9ff
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 22:42:04 -0600

    Added Name Variable

---
+++ FallClass/Assets/VariableTest1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableTest1 : MonoBehaviour
+namespace MyApplication
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string name = "John";
+         Console.WriteLine(name);
+      }
+   }
+}
