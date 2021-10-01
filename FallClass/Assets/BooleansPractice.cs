d856993c519e2e2a72c6dca9d46fd4ba13af4513
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:39:09 -0600

    Added Booleans Practice

---
+++ FallClass/Assets/BooleansPractice.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooleansPractice : MonoBehaviour
+{
+
+    private bool isCodingFun = true;
+    private bool isFishTasty = false;
+    Connsole.WriteLine(isCodingFun);
+    Console.WriteLine(isFishTasty);
+    private int x = 10;
+    private int y = 9;
+    Console.Writeline(x > y);
+}
+   
