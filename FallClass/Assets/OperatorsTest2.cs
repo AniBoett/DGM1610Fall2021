1d6fe9272a8a81d032e7f4444c035cfe234740dd
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:28:52 -0600

    Added Sum Operator 2

---
+++ FallClass/Assets/OperatorsTest2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorsTest2 : MonoBehaviour
+{
+namespace MyApplication
+{ 
+class Program
+{
+static void Main(string[] args)
+{
+  int x = 5;
+int y = 3; 
+Console.WriteLine(x+y);
+}
+}
+}
+}
