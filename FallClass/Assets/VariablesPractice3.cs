29c95dcb34b9825e8e2992685e16c5971767917a
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:15:41 -0600

    Added Practice Variables 3

---
+++ FallClass/Assets/VariablesPractice3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesPractice3 : MonoBehaviour
+{
+  Console.WriteLine("Enter username:");
+  private string userName = Console.ReadLine();
+  Console.WriteLine("Username is: " + userName);
+    Console.WriteLine("Think of a number:");
+    private int yNum = Convert.ToInt32(Colsole.ReadLine());
+    Console.WriteLine("Your number is: " + myNum);
+}
