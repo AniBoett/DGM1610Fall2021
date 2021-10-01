b86bfd2e0de7393b18917e9c4f15b6ac9d0e93bf
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:54:13 -0600

    Tested Multi-Line comment
    tested with /* and */

--- FallClass/Assets/Practice1.cs
+++ FallClass/Assets/Practice1.cs
@@ -6,6 +6,7 @@ public class Practice1 : MonoBehaviour
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello World!");
+    Console.WriteLine("Hello World!"); /*Testing multi-line comment since I've never used it before
+    this is how to do a multi line comment*/
   }
 }
