544b70cbf4648bac7dfef368badea4d2d465184a
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:33:47 -0600

    Added numbers to variable

---
+++ FallClass/Assets/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   private int x = 10;
+   int += 5;
+      Console.Writeline(x)
+}
