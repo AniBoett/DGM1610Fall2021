0f3fa2bbe3a50e506fff6c9ef40c1240d8f08a3f
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:52:48 -0600

    Added Practice HelloWorld Test

---
+++ FallClass/Assets/Practice1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice1 : MonoBehaviour
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+  }
+}
