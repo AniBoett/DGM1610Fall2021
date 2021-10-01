a3cf7fb77711fd122f56a60795337fd1491f1ab7
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 23:05:49 -0600

    Tested Float

---
+++ FallClass/Assets/Floattest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floattest : MonoBehaviour
+{
+    private float myNum = 5.75F;
+    Console.WriteLine(myNum);
+}
