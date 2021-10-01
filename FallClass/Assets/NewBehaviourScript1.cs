863866d5831daa48d99e753f3bac455f080f299d
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:42:20 -0600

    Added Comparison Operators

---
+++ FallClass/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    private int x = 5;
+    private int x = y;
+   Console.WriteLine(x == y); // returns False since x and y aren't equal
+   Console.WriteLine(x > y); // returns True because 5 is greater than 3
+   Console.WriteLine(x != y); // returns True because x and y aren't equal
+   Console.WriteLine(x < y); // returns False since 5 is greater than 3
+   Console.WriteLine(x >= y); // returns True because 5 is greater than 3
+   Console.WriteLine(x ,= y); // returns False because 5 is greater than 3
+
+}
