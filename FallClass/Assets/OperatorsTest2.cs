536a879289894634f31027c6b636cec975bf5bb1
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:31:10 -0600

    Added other operators
    Added to OperatorsTest2

--- FallClass/Assets/OperatorsTest2.cs
+++ FallClass/Assets/OperatorsTest2.cs
@@ -12,7 +12,14 @@ static void Main(string[] args)
 {
   int x = 5;
 int y = 3; 
+x++;
+x--;
 Console.WriteLine(x+y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.Writeline(x / y);
+Console.Writeline(x % y);
+Console.Writeline(x)
 }
 }
 }
