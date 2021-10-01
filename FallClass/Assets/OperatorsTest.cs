998ad92476afd9f449a4a1b089e24d422d5cc496
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 23:13:29 -0600

    Tested Sum Operator

---
+++ FallClass/Assets/OperatorsTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorsTest : MonoBehaviour
+{
+    private int sum1 = 100 + 50; //150 (100 + 50)
+    private int sum2 = sum1 + 250; // 400 (150 + 250)
+    private int sum3 = sum2 + sum2; // 800 (400 + 400)
+    private int sum4 = 1 + sum3; // 801 (800 + 1) Personal Test
+}
