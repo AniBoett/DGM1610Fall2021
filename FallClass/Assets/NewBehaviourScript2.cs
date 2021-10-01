97f2551f7f06d93e4cd44c9c71520e4f8b325e0f
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:00:17 -0600

    Tested Comma separated list

---
+++ FallClass/Assets/NewBehaviourScript2.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour
+{
+   private int x = 5, y = 6, z = 50;
+   Console.WriteLine(x + y + z);
+}
