d237fd6a04d16f4a0570b8f1eabf28fcac544d2a
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:46:26 -0600

    Added If Else practice

---
+++ FallClass/Assets/IfElsePractice.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElsePractice : MonoBehaviour
+{
+   private int x = 30;
+   private int y = 2;
+   if (x > y)
+   {
+      Console.WriteLine("Congratulations")
+   }
+      int z = 1
+         int a = 1
+            if (z == a)
+         {
+            Console.WriteLine("Hello World")
+         }
+            else
+{
+Console.writeLine("Goodbye World")
+}
+            
+}
