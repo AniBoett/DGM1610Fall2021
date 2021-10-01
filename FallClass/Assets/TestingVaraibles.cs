844438083f4fce03c563d46d369f104682ec6ef6
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 22:55:46 -0600

    Testing Different Operators
    Tried multiple different operators and variables

---
+++ FallClass/Assets/TestingVaraibles.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingVaraibles : MonoBehaviour
+{
+    private int myNum = 5; // Integer (whole number)
+    private double myDoubleNum = 5.99D; // Floating point number
+    private char myLetter = 'D'; // Character stores one letter
+    private bool myBool = true; // Boolean stores true or false data
+    private string myText = "Hello"; // String stores a sequence of characters 
+}
