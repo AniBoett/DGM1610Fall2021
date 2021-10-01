7a0aa7a64dd7fb37853964bc96e9dd8c5d44003b
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:21:35 -0600

    Added Operators Practice 1

---
+++ FallClass/Assets/PracticeOperators1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeOperators1 : MonoBehaviour
+{
+Console.WriteLine(10 * 5);
+Console.WriteLine(10 / 5);
+private int x = 10;
+int++;
+    x += 5
+        
+}
