9b0c26495ff228980df33be223e464eba18519dd
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 23:02:49 -0600

    Added Int Test

---
+++ FallClass/Assets/IntTest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntTest : MonoBehaviour
+{
+  private int myNum = 100000;
+  Console.WriteLine(myNum);
+}
