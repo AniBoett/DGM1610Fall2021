d1cb024aa9e4620aad20f351b9c9d3e04bf9d8da
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 22:47:56 -0600

    Number Variable Method 2
    tested without intro script seen in try it yourself tab

---
+++ FallClass/Assets/NumberVariable2.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberVariable2 : MonoBehaviour
+{
+    private int mynum;
+    myNum = 15;
+        Console.WriteLine(myNum);
+}
