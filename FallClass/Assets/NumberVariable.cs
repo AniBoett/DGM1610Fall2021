4756d15c600ce1094cc78f35f75c3a695b0f9427
Anika Boettcher <10945351@my.uvu.edu>
2021-09-30 22:46:14 -0600

    Added Number Variable

---
+++ FallClass/Assets/NumberVariable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberVariable : MonoBehaviour
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 15;
+            Console.WriteLine(myNum);
+        }
+    }
+}
