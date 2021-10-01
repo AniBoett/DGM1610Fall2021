a44a566645b4cc2201cb264e48f506f95f92071e
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:08:35 -0600

    Added PracticeVariables2
    includes bool and string

---
+++ FallClass/Assets/PracticeVariables2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeVariables2 : MonoBehaviour
+{
+ private bool yay = true;
+ private bool nay = false;
+ private string greeting = "Hello";
+ Console.WriteLine(greeting);
+ 
+}
