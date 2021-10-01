39b53f371db498cc730b13974d127b1378528012
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 14:25:34 -0600

    Added Math Practice

---
+++ FallClass/Assets/MathPractice1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathPractice1 : MonoBehaviour
+{
+ private int x = 5;
+ private int y = 10;
+ COnsole.WriteLine(Math.Max(x, y));;
+  Console.WriteLine(Math.Sqrt(64)); //sqrt finds square roots
+      Console.WriteLine(Math.Round(2.6)); //Round rounds a decimal to a whole number
+}
+
