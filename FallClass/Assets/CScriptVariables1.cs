8d041af6f88d37115675820e99227434f0808449
Anika Boettcher <10945351@my.uvu.edu>
2021-10-01 13:58:50 -0600

    Added Practice Variables1

---
+++ FallClass/Assets/CScriptVariables1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CScriptVariables1 : MonoBehaviour
+{
+    int myNum = 50
+    string myName = "John"
+    private int x = 5;
+    private int y = 10;
+    Console.WriteLine(x + y);
+    private int z = (x + y);
+    Console.WriteLine(z);
+
+}
