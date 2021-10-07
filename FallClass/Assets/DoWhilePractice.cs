19981e9ba860df782f92b5515d119788f82b16e2
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 14:29:53 -0600

    Added DoWhilePractice

---
+++ FallClass/Assets/DoWhilePractice.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhilePractice : MonoBehaviour
+{
+ int i = 0
+  do
+
+ {
+  Console.WriteLine(i); //code block to be executed
+  i++;
+ }
+
+while (i < 5) ;
+}
