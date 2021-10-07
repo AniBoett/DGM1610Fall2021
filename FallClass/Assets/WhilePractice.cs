90b47730673e19f334b85f49fbda41ba1bcf9631
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 14:01:29 -0600

    Added While Loop Practice 1

---
+++ FallClass/Assets/WhilePractice.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhilePractice : MonoBehaviour
+{
+  while (condition)
+  {
+// code to be executed during condition
+}
+
+
+int i = 0
+while (i < 5)
+{
+Console.WriteLine(i);
+i++;
+}
+}
