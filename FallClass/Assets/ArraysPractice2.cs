4a3f41a34b16373ce55cf92d28917db18c352893
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 20:36:01 -0600

    Added Arrays Practice 2

---
+++ FallClass/Assets/ArraysPractice2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysPractice2 : MonoBehaviour
+{
+ private string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Clown Car"};
+ foreach (string i in cars)
+ {
+  Console.WriteLine(i)
+ }
+}
