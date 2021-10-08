23173b14d041ab0e198222731513573533577cc0
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 20:41:50 -0600

    Modified Arrays Practice 1

--- FallClass/Assets/ArraysPractice.cs
+++ FallClass/Assets/ArraysPractice.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ArraysPractice : MonoBehaviour
 {
     private string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";
     for (int i = 0; i<cars.Length; i++)
     {
         Console.WriteLine(cars[1]);
