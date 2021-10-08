a1523e7bb79cd04cc4e703224af627b09c177df5
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 20:29:41 -0600

    Added For While Practice

---
+++ FallClass/Assets/ForWhilePractice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForWhilePractice : MonoBehaviour
+{
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i) //Codeblock executed under above conditions
+}
+}
