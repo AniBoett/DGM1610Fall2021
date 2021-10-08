8260de5afbc93dc830c9a213c011b2fa09703437
Anika Boettcher <10945351@my.uvu.edu>
2021-10-07 20:43:31 -0600

    Modified Arrays Practice 2
    Added array sort

--- FallClass/Assets/ArraysPractice2.cs
+++ FallClass/Assets/ArraysPractice2.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ArraysPractice2 : MonoBehaviour
 {
  private string[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Clown Car"};
+  Array.Sort(cars);
  foreach (string i in cars)
  {
   Console.WriteLine(i)
